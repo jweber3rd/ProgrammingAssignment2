28996c8a10429a5e0cb9803dfcb764a28f4b23f1
John Weber <jweber3rd@users.noreply.github.com>
2014-05-25 19:58:15 -0400

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,11 @@
 cacheSolve <- function(x=matrix(), ...) {
         m <- x$getmatrix()
-        if(!is.null(m)) {
+        if(!is.null(m)) { # checks to see if the inverse matrix has already been calculated
                 message("getting cached inverse matrix")
-                return(m)
+                return(m) # if inverse matrix exists, returns inverse matrix from the cache and skips the computation
         }
         matrix <- x$get()
-        m <- mean(matrix, ...)
-        x$setmatrix(m)
+        m <- solve(matrix, ...) # if inverse matrix does not exist, computes the inverse matrix
+        x$setmatrix(m) # sets the value of the inverse matrix in the cache via the setmatrix function.
         m
 }
